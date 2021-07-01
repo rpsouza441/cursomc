f8fbdc3cd2875509a4231207fb088504035696b3
Rodrigo <rpsouza@hotmail.co.uk>
2021-06-30 21:47:57 -0300

    categorais

--- src/main/java/org/rodrigo/cursomc/resources/CategoriaResource.java
+++ src/main/java/org/rodrigo/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,16 @@
 package org.rodrigo.cursomc.resources;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public String listar() {
+
 		return "ok";
 	}
 
