28ff37c33769a30a2757f28f452de1e17d314692
Rodrigo <rpsouza@hotmail.co.uk>
2021-06-30 21:39:09 -0300

    first commit

---
+++ src/main/java/org/rodrigo/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package org.rodrigo.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "ok";
+	}
+
+}
